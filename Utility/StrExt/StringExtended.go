9807f6805159e600362599936211649b3af1017b
PlayerR9 <riateapad@gmail.com>
2024-01-06 12:42:57 +0100

    Update

--- Utility/StrExt/StringExtended.go
+++ Utility/StrExt/StringExtended.go
@@ -32,37 +32,82 @@ func ReplaceSuffix(str, suffix string) (string, error) {
 	return str[:len(str)-len(suffix)] + suffix, nil
 }
 
-func FindContentIndexes(openingToken, closingToken string, contentTokens []string) (int, int, bool) {
+// FindContentIndexes searches for the positions of opening and closing tokens
+// in a slice of strings.
+// It returns the start and end indexes of the content between the tokens, and
+// an error if any.
+//
+// Parameters:
+//
+//   - openingToken: The string that marks the beginning of the content.
+//   - closingToken: The string that marks the end of the content.
+//   - contentTokens: The slice of strings in which to search for the tokens.
+//
+// Returns:
+//   - The start index of the content (inclusive).
+//   - The end index of the content (exclusive).
+//   - An error if the opening or closing tokens are empty, or if they are not
+//     found in the correct order in the contentTokens.
+//
+// If the openingToken is found but the closingToken is not, the function will
+// return an error.
+// If the closingToken is found before the openingToken, the function will
+// return an error.
+// If the closingToken is a newline ("\n") and it is not found, the function will
+// return the length of the contentTokens as the end index.
+//
+// Errors returned can be of type ErrOpeningTokenEmpty, ErrClosingTokenEmpty,
+// ErrOpeningTokenNotFound, or a generic error with a message about the closing token.
+func FindContentIndexes(openingToken, closingToken string, contentTokens []string) (int, int, error) {
 	if openingToken == "" {
-		panic("Invalid input: Opening token cannot be empty")
+		return 0, 0, &ErrOpeningTokenEmpty{}
 	}
-
 	if closingToken == "" {
-		panic("Invalid input: Closing token cannot be empty")
+		return 0, 0, &ErrClosingTokenEmpty{}
 	}
 
-	startIndex := slices.Index(contentTokens, openingToken)
-	if startIndex == -1 {
-		panic("Invalid input: Opening token not found in the content")
+	openingTokenIndex := slices.Index(contentTokens, openingToken)
+	if openingTokenIndex < 0 {
+		return 0, 0, &ErrOpeningTokenNotFound{}
 	}
 
-	tokenCounter := 1
-	for i, token := range contentTokens[startIndex+1:] {
-		switch token {
-		case closingToken:
-			tokenCounter--
-		case openingToken:
-			tokenCounter++
+	tokenStartIndex := openingTokenIndex + 1
+
+	tokenBalance := 1
+	closingTokenIndex := slices.IndexFunc(contentTokens[tokenStartIndex:], func(token string) bool {
+		if token == closingToken {
+			tokenBalance--
+		} else if token == openingToken {
+			tokenBalance++
 		}
 
-		if tokenCounter == 0 {
-			return startIndex, startIndex + i + 1, true
-		}
-	}
+		return tokenBalance == 0
+	})
 
-	return startIndex, len(contentTokens) - 1, closingToken == "\n"
+	if closingTokenIndex != -1 {
+		return tokenStartIndex, tokenStartIndex + closingTokenIndex + 1, nil
+	} else if tokenBalance < 0 {
+		return 0, 0, fmt.Errorf("closing token '%s' not opened", closingToken)
+	} else if tokenBalance == 1 && closingToken == "\n" {
+		return tokenStartIndex, len(contentTokens), nil
+	} else {
+		return 0, 0, fmt.Errorf("closing token '%s' not found", closingToken)
+	}
 }
 
+// GetOrdinalSuffix returns the ordinal suffix for a given integer.
+//
+// Parameters:
+//   - number: The integer for which to get the ordinal suffix.
+//
+// The function returns a string that represents the number with its ordinal suffix.
+//
+// For example, for the number 1, the function returns "1st"; for the number 2, it returns "2nd"; and so on.
+// For numbers ending in 11, 12, or 13, the function returns the number with the suffix "th" (e.g., "11th", "12th", "13th").
+// For negative numbers, the function also returns the number with the suffix "th".
+//
+// If the last digit of the number is 0 or greater than 3 (and the last two digits are not 11, 12, or 13), the function returns the number with the suffix "th".
+// If the last digit of the number is 1, 2, or 3 (and the last two digits are not 11, 12, or 13), the function returns the number with the corresponding ordinal suffix ("st", "nd", or "rd").
 func GetOrdinalSuffix(number int) string {
 	if number < 0 {
 		return fmt.Sprintf("%dth", number)
--- Utility/StrExt/errors.go
+++ Utility/StrExt/errors.go
@@ -5,3 +5,21 @@ type ErrSuffixTooLong struct{}
 func (e ErrSuffixTooLong) Error() string {
 	return "suffix is too long"
 }
+
+type ErrOpeningTokenEmpty struct{}
+
+func (e ErrOpeningTokenEmpty) Error() string {
+	return "opening token is empty"
+}
+
+type ErrClosingTokenEmpty struct{}
+
+func (e ErrClosingTokenEmpty) Error() string {
+	return "closing token is empty"
+}
+
+type ErrOpeningTokenNotFound struct{}
+
+func (e ErrOpeningTokenNotFound) Error() string {
+	return "opening token not found in content"
+}
