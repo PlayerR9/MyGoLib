debb7003c832747372b2466ec07bf309b9369b76
PlayerR9 <riateapad@gmail.com>
2024-02-09 23:34:03 +0100

    update

--- Utility/Errors/go_routines.go
+++ Utility/Errors/go_routines.go
@@ -5,14 +5,152 @@ import (
 	"sync"
 )
 
-type GRHandler struct {
+type RecoverBuilder struct {
+}
+
+func (b *RecoverBuilder) Build() func() {
+	return func() {
+		r := recover()
+		if r == nil {
+			// success
+			return
+		}
+
+		if x, ok := r.(error); ok {
+			// error
+		} else {
+			// panic
+		}
+	}
+}
+
+func RecoverGeneral() {
+	r := recover()
+	if r == nil {
+		// success
+		// 1. Ignore: return //
+		// 2. Handle: success()
+		// 3. Default: default()
+		return
+	}
+
+	if x, ok := r.(error); ok {
+		// error
+		// 1. Forwards: panic(x)
+		// 2. Handle: error(x)
+		// 3. Default: default() //
+	} else {
+		// panic:
+		// 1. Forwards: panic(r) //
+		// 2. Handle: err = &ErrPanic{value: r}
+	}
+}
+
+// builder -> function
+
+type Y struct {
+	status error
+
+	wg sync.WaitGroup
+
+	whenSuccess func()
+	whenDone    func(error) // or when error
+}
+
+func NewSimple(whenDone func(error)) *Y {
+	y := &Y{}
+
+	if whenDone == nil {
+		y.whenDone = func(err error) {
+			y.status = err
+		}
+	} else {
+		y.whenDone = whenDone
+	}
+
+	return y
+}
+
+func NewComplex(whenError func(error), whenSuccess func()) *Y {
+	y := &Y{}
+
+	if whenError == nil {
+		y.whenDone = func(err error) {
+			y.status = err
+		}
+	} else {
+		y.whenDone = whenError
+	}
+
+	if whenSuccess != nil {
+		y.whenSuccess = whenSuccess
+	} else {
+		y.whenSuccess = func() {
+			y.status = nil
+		}
+	}
+
+	return y
+}
+
+func (y *Y) Run(z func(), routine func()) {
+	y.wg.Add(1)
+
+	go func() {
+		defer y.wg.Done()
+
+		defer func() {
+			r := recover()
+			if r == nil {
+				if y.whenSuccess != nil {
+					y.whenSuccess()
+				} else {
+					y.whenDone(nil)
+				}
+
+				return
+			}
+
+			// h.mu.Lock()
+			// defer h.mu.Unlock()
+
+			var err error
+
+			if x, ok := r.(error); ok {
+				err = x
+			} else {
+				err = &ErrPanic{value: r}
+			}
+
+			y.whenDone(err)
+		}()
+
+		routine()
+	}()
+}
+
+type GRHandler interface {
+	RunF(func())
+}
+
+type SimpleGRH struct {
 	wg sync.WaitGroup
 
 	status error
 	mu     sync.RWMutex
 
-	id       string
-	whenDone func(error)
+	id string
+}
+
+type ComplexGRH struct {
+	wg sync.WaitGroup
+
+	status error
+	mu     sync.RWMutex
+
+	id          string
+	whenSuccess func()
+	whenError   func(error)
 }
 
 func (h *GRHandler) Wait() error {
@@ -24,78 +162,25 @@ func (h *GRHandler) Wait() error {
 	return h.status
 }
 
-type GRHandlerOption func(*GRHandler)
-
-func WithIdentifier(id string) GRHandlerOption {
-	return func(h *GRHandler) {
-		h.id = id
-	}
-}
-
-func WhenDone(whenDone func(error)) GRHandlerOption {
-	return func(h *GRHandler) {
-		h.whenDone = whenDone
-	}
-}
-
-func WhenPanic(whenPanic func(interface{})) GRHandlerOption {
-	return func(h *GRHandler) {
-	}
-}
-
-func WhenError(whenError func(error)) GRHandlerOption {
-	return func(h *GRHandler) {
-	}
-
-}
-
-func WhenSuccess(whenSuccess func()) GRHandlerOption {
-	return func(h *GRHandler) {
-	}
-}
-
-func GoStart(id string, whenDone func(error), routine func()) *GRHandler {
-	h := &GRHandler{
-		status: nil,
-		id:     id,
-	}
-
-	id
-	done
-	panic
-	err
-	success
-	routine
-
-	var whenDone func(error)
-
-	var whenSuccess func()
-	var whenError func(error)
-	var whenPanic func(interface{})
-
-	r := recover()
-	if r == nil {
-		whenSuccess() || whenDone()
-
-		return
-	}
-
-	h.mu.Lock()
-	defer h.mu.Unlock()
-
-	switch x := r.(type) {
-	case error:
-		h.whenError(x) || h.whenDone(x)
-	default:
-		h.whenPanic(r) || h.whenDone(&ErrPanic{value: r})
-	}
+func NewGRHandler(whenDone func(error)) *GRHandler {
+	var h *GRHandler
 
 	if whenDone == nil {
-		h.whenDone = func(err error) {
-			h.status = err
+		h = &GRHandler{
+			whenSuccess: func() {
+				h.status = nil
+			},
+			whenError: func(err error) {
+				h.status = err
+			},
 		}
 	} else {
-		h.whenDone = whenDone
+		h = &GRHandler{
+			whenSuccess: func() {
+				whenDone(nil)
+			},
+			whenError: whenDone,
+		}
 	}
 
 	h.wg.Add(1)
@@ -106,8 +191,11 @@ func GoStart(id string, whenDone func(error), routine func()) *GRHandler {
 		defer func() {
 			r := recover()
 			if r == nil {
-				whenSuccess()
-				whenDone()
+				if whenSuccess != nil {
+					whenSuccess()
+				} else {
+					whenDone(nil)
+				}
 
 				return
 			}
@@ -115,13 +203,18 @@ func GoStart(id string, whenDone func(error), routine func()) *GRHandler {
 			h.mu.Lock()
 			defer h.mu.Unlock()
 
-			switch x := r.(type) {
-			case error:
-				h.whenError(x)
-				h.whenDone(x)
-			default:
-				h.whenPanic(r)
-				h.whenDone(&ErrPanic{value: r})
+			var err error
+
+			if x, ok := r.(error); ok {
+				err = x
+			} else {
+				err = &ErrPanic{value: r}
+			}
+
+			if whenError != nil {
+				whenError(err)
+			} else {
+				whenDone(err)
 			}
 		}()
 
@@ -129,6 +222,196 @@ func GoStart(id string, whenDone func(error), routine func()) *GRHandler {
 	}()
 
 	return h
+
+	/*
+
+		h := &GRHandler{
+			status: nil,
+			id:     id,
+		}
+
+		/*
+			id
+			done
+			panic
+			err
+			success
+			routine
+
+
+		var whenSuccess func()
+		var whenError func(error)
+		var whenPanic func(interface{})
+
+	*/
+}
+
+func GoStartWithOptions(whenSuccess func(), whenError func(error), whenPanic func(any)) *GRHandler {
+	h := &GRHandler{}
+
+	if whenDone != nil {
+		h.whenDone = whenDone
+	} else {
+		h.whenDone = func(err error) {
+			h.status = err
+		}
+	}
+
+	h.wg.Add(1)
+
+	go func() {
+		defer h.wg.Done()
+
+		defer func() {
+			r := recover()
+			if r == nil {
+				if whenSuccess != nil {
+					whenSuccess()
+				} else {
+					whenDone(nil)
+				}
+
+				return
+			}
+
+			h.mu.Lock()
+			defer h.mu.Unlock()
+
+			if whenPanic == nil {
+				switch x := r.(type) {
+				case error:
+					if whenError != nil {
+						whenError(x)
+					} else {
+						whenDone(x)
+					}
+				default:
+					whenDone(&ErrPanic{value: r})
+				}
+			} else {
+				switch x := r.(type) {
+				case error:
+					if whenError != nil {
+						whenError(x)
+					} else {
+						whenDone(x)
+					}
+				default:
+					whenPanic(r)
+				}
+			}
+		}()
+
+		routine()
+	}()
+
+	return h
+
+	/*
+
+		h := &GRHandler{
+			status: nil,
+			id:     id,
+		}
+
+		/*
+			id
+			done
+			panic
+			err
+			success
+			routine
+
+
+		var whenSuccess func()
+		var whenError func(error)
+		var whenPanic func(interface{})
+
+	*/
+}
+
+func (h *GRHandler) Run(routine func()) *GRHandler {
+	h := &GRHandler{}
+
+	if whenDone != nil {
+		h.whenDone = whenDone
+	} else {
+		h.whenDone = func(err error) {
+			h.status = err
+		}
+	}
+
+	h.wg.Add(1)
+
+	go func() {
+		defer h.wg.Done()
+
+		defer func() {
+			r := recover()
+			if r == nil {
+				if whenSuccess != nil {
+					whenSuccess()
+				} else {
+					whenDone(nil)
+				}
+
+				return
+			}
+
+			h.mu.Lock()
+			defer h.mu.Unlock()
+
+			if whenPanic == nil {
+				switch x := r.(type) {
+				case error:
+					if whenError != nil {
+						whenError(x)
+					} else {
+						whenDone(x)
+					}
+				default:
+					whenDone(&ErrPanic{value: r})
+				}
+			} else {
+				switch x := r.(type) {
+				case error:
+					if whenError != nil {
+						whenError(x)
+					} else {
+						whenDone(x)
+					}
+				default:
+					whenPanic(r)
+				}
+			}
+		}()
+
+		routine()
+	}()
+
+	return h
+
+	/*
+
+		h := &GRHandler{
+			status: nil,
+			id:     id,
+		}
+
+		/*
+			id
+			done
+			panic
+			err
+			success
+			routine
+
+
+		var whenSuccess func()
+		var whenError func(error)
+		var whenPanic func(interface{})
+
+	*/
 }
 
 func (h *GRHandler) PanicIf() {
