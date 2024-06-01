65908c305b1e73fbe584691b9f9e1c439472b377
Player R9 <riateapad@gmail.com>
2024-06-01 12:12:19 +0200

    update

--- Units/Common/other_interfaces.go
+++ Units/Common/other_interfaces.go
@@ -250,3 +250,105 @@ func RunesOf(str any) []rune {
 		return []rune(fmt.Sprintf("%v", str))
 	}
 }
+
+// Hashable is an interface that provides a method to get the hash code of an element.
+type Hashable interface {
+	// Hash returns the hash code of the element.
+	//
+	// Returns:
+	//   - int: The hash code of the element.
+	Hash() int
+}
+
+// HashCode returns the hash code of an element. If the element implements the Hashable interface,
+// the Hash method is called. Otherwise, the hash code is calculated using the default hash function.
+//
+// Parameters:
+//   - elem: The element to get the hash code from.
+//
+// Returns:
+//   - int: The hash code of the element.
+func HashCode(elem any) int {
+	if elem == nil {
+		return 0
+	}
+
+	switch elem := elem.(type) {
+	case Hashable:
+		return elem.Hash()
+	case int:
+		return elem
+	case int8:
+		return int(elem)
+	case int16:
+		return int(elem)
+	case int32:
+		return int(elem)
+	case int64:
+		return int(elem)
+	case uint:
+		return int(elem)
+	case uint8:
+		return int(elem)
+	case uint16:
+		return int(elem)
+	case uint32:
+		return int(elem)
+	case uint64:
+		return int(elem)
+	case float32:
+		return int(elem)
+	case float64:
+		return int(elem)
+	case bool:
+		if elem {
+			return 1
+		} else {
+			return 0
+		}
+	case string:
+		return hashString(elem)
+	case []byte:
+		return hashBytes(elem)
+	case error:
+		return hashString(elem.Error())
+	case fmt.Stringer:
+		return hashString(elem.String())
+	default:
+		return hashString(fmt.Sprintf("%v", elem))
+	}
+}
+
+// hashString is a helper function that calculates the hash code of a string.
+//
+// Parameters:
+//   - str: The string to calculate the hash code from.
+//
+// Returns:
+//   - int: The hash code of the string.
+func hashString(str string) int {
+	hash := 0
+
+	for _, r := range str {
+		hash = 31*hash + int(r)
+	}
+
+	return hash
+}
+
+// hashBytes is a helper function that calculates the hash code of a byte slice.
+//
+// Parameters:
+//   - bytes: The byte slice to calculate the hash code from.
+//
+// Returns:
+//   - int: The hash code of the byte slice.
+func hashBytes(bytes []byte) int {
+	hash := 0
+
+	for _, b := range bytes {
+		hash = 31*hash + int(b)
+	}
+
+	return hash
+}
