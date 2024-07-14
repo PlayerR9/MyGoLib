583f271fc9b9655867bb0f75a84e1be699244a8a
PlayerR9 <riateapad@gmail.com>
2024-07-14 18:26:25 +0200

    update

--- go_generator/common.go
+++ go_generator/common.go
@@ -2,6 +2,7 @@ package go_generator
 
 import (
 	"errors"
+	"fmt"
 	"go/build"
 	"path/filepath"
 	"slices"
@@ -301,4 +302,96 @@ func IsValidName(variable_name string, keywords []string, exported GoExport) err
 	}
 
 	return nil
+}
+
+func MakeParameterList(fields map[string]string) (string, error) {
+	var field_list []string
+	var type_list []string
+
+	for k, v := range fields {
+		if k == "" {
+			err := errors.New("found type name with empty name")
+			return "", err
+		}
+
+		first_letter := rune(k[0])
+
+		ok := unicode.IsLetter(first_letter)
+		if !ok {
+			err := fmt.Errorf("type name %q must start with a letter", k)
+			return "", err
+		}
+
+		ok = unicode.IsUpper(first_letter)
+		if !ok {
+			continue
+		}
+
+		pos, ok := slices.BinarySearch(field_list, k)
+		uc.AssertF(!ok, "%q must be unique", k)
+
+		field_list = slices.Insert(field_list, pos, k)
+		type_list = slices.Insert(type_list, pos, v)
+	}
+
+	var values []string
+	var builder strings.Builder
+
+	for i := 0; i < len(field_list); i++ {
+		param := strings.ToLower(field_list[i])
+
+		builder.WriteString(param)
+		builder.WriteRune(' ')
+		builder.WriteString(type_list[i])
+
+		str := builder.String()
+		values = append(values, str)
+
+		builder.Reset()
+	}
+
+	joined_str := strings.Join(values, ", ")
+
+	return joined_str, nil
+}
+
+func MakeAssignmentList(fields map[string]string) (map[string]string, error) {
+	var field_list []string
+	var type_list []string
+
+	for k, v := range fields {
+		if k == "" {
+			err := errors.New("found type name with empty name")
+			return nil, err
+		}
+
+		first_letter := rune(k[0])
+
+		ok := unicode.IsLetter(first_letter)
+		if !ok {
+			err := fmt.Errorf("type name %q must start with a letter", k)
+			return nil, err
+		}
+
+		ok = unicode.IsUpper(first_letter)
+		if !ok {
+			continue
+		}
+
+		pos, ok := slices.BinarySearch(field_list, k)
+		uc.AssertF(!ok, "%q must be unique", k)
+
+		field_list = slices.Insert(field_list, pos, k)
+		type_list = slices.Insert(type_list, pos, v)
+	}
+
+	assignment_map := make(map[string]string)
+
+	for i := 0; i < len(field_list); i++ {
+		param := strings.ToLower(field_list[i])
+
+		assignment_map[field_list[i]] = param
+	}
+
+	return assignment_map, nil
 }
